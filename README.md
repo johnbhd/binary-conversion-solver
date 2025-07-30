4892a0ca6564fd21c07e5500e0ecc05044f0dec1
johnbhd <villegasjohnb18@gmail.com>
2025-07-30 19:48:02 +0800

    structure form

---
+++ README.md
@@ -0,0 +1,16 @@
+# Binary Conversion Solver
+- A binary conversion that compute problems with solution
+
+## Conversion
+- Decimal
+- Octal
+- Hexadecimal
+
+
+## Tech Stack
+- React Vite
+- Tailwind CSS
+
+
+
+#### Made by [johnbhd](https://github.com/johnbhd)
---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react"
+
+function App() {
+  const [form, setForm] = useState({ 
+    input: 0
+  })
+
+  const handleChange = (e) => { 
+    setForm({
+      ...form,
+      [e.target.name] : e.target.value
+    })
+  }
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(form.input)
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen space-y-2">
+      <h2>Binary Conversion Solver - {form.input}</h2>
+      
+      <form className="space-y-4" onSubmit={handleSubmit}>
+        <div className="flex gap-9">
+          <div className="flex flex-col gap-y-2">
+            <label>From:</label>
+            <select id="" className="bg-gray-100 p-2 cursor-pointer outline-0">
+              <option value="">Binary</option>
+              <option value="">Decimal</option>
+              <option value="">Octal</option>
+              <option value="">Hexadecimal</option>
+            </select>
+          </div>
+
+          <div className="flex flex-col gap-y-2">
+            <label>To:</label>
+            <select id="" className="bg-gray-100 p-2 cursor-pointer outline-0">
+              <option value="">Binary</option>
+              <option value="">Decimal</option>
+              <option value="">Octal</option>
+              <option value="">Hexadecimal</option>
+            </select>
+            
+          </div>
+        </div>
+        <div className="flex flex-col gap-y-4">
+          <input 
+            type="number" 
+            name="input"
+            value={form.input}
+            onChange={handleChange}
+            placeholder="Enter Number" 
+            className="px-2 m-auto border" 
+          />
+          <button className="bg-blue-200 p-1 rounded-lg cursor-pointer" type="submit">Calculate</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default App
